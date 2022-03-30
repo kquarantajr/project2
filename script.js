761a08241f9ae9a1cdb43b88396ae45fb0bb4ba5
Kevin Quaranta Jr <kquarantajr@gmail.com>
2022-03-30 10:41:14 -0700

    failed idea, deleting second.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const generateButton = document.querySelector('#generateButton');
-const pwDisplay = document.querySelector('#pwDisplay');
 const resetButton = document.querySelector('#resetButton');
+const pwDisplay = document.querySelector('#pwDisplay');
 
 //Input Fields
 let numbersRadios = document.body.querySelectorAll('input[name="numbers"]');
@@ -29,35 +29,32 @@ resetButton.addEventListener('click', () => {
   pwDisplay.innerText = '';
 });
 
-generateButton.addEventListener('click', async () => {
-    let numbersValue = false;
-    let specialValue = false;
-    let uppercaseValue = false;
+generateButton.addEventListener('click', async() => {
+  let numbersValue = false;
+  let specialValue = false;
+  let uppercaseValue = false;
 
-    if(numbersRadios[0].checked == true){
-      numbersValue = 'num=true&';
-    }else{
-      numbersValue = '';
-    }
-    if(specialRadios[0].checked == true){
-      specialValue = 'char=true&';
-    }else{
-      specialValue = '';
-    }
-    if(uppercaseRadios[0].checked == true){
-      uppercaseValue = 'caps=true&';
-    } else{
-      uppercaseValue = '';
-    }
-
-
-    //GET Request
-    let response = await axios.get(
-        `https://passwordinator.herokuapp.com?${numbersValue}${specialValue}${uppercaseValue}len=${length.value}`
-      )
-
-    console.log(response);
-    let pw = response.data.data;
-    pwDisplay.innerText = pw;
+  if(numbersRadios[0].checked == true){
+    numbersValue = 'num=true&';
+  }else{
+    numbersValue = '';
   }
-)
+  if(specialRadios[0].checked == true){
+    specialValue = 'char=true&';
+  }else{
+    specialValue = '';
+  }
+  if(uppercaseRadios[0].checked == true){
+    uppercaseValue = 'caps=true&';
+  } else{
+    uppercaseValue = '';
+  }
+
+  //GET Request
+  response = await axios.get(
+      `https://passwordinator.herokuapp.com?${numbersValue}${specialValue}${uppercaseValue}len=${length.value}`
+    )
+
+  let pw = response.data.data;
+  pwDisplay.innerText = pw;
+});
